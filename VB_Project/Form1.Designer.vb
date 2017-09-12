71650d7a35faba36f246daf9921753542e54f914
Romeu Andrade Cavalcante <romeuac@gmail.com>
2017-09-12 00:04:05 -0300

    small application to review concepts

--- VB_Project/Form1.Designer.vb
+++ VB_Project/Form1.Designer.vb
@@ -22,9 +22,64 @@ Partial Class Form1
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.btnOk = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(13, 13)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(85, 13)
+        Me.lblName.TabIndex = 0
+        Me.lblName.Text = "Insert your name"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(16, 29)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(218, 20)
+        Me.txtName.TabIndex = 1
+        '
+        'btnOk
+        '
+        Me.btnOk.Location = New System.Drawing.Point(240, 29)
+        Me.btnOk.Name = "btnOk"
+        Me.btnOk.Size = New System.Drawing.Size(42, 20)
+        Me.btnOk.TabIndex = 2
+        Me.btnOk.Text = "O&K"
+        Me.btnOk.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(197, 226)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 3
+        Me.btnClose.Text = "C&lose"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnOk)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.lblName)
+        Me.Name = "Form1"
+        Me.Text = "Hello World!"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents lblName As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents btnOk As Button
+    Friend WithEvents btnClose As Button
 End Class
--- VB_Project/Form1.vb
+++ VB_Project/Form1.vb
@@ -1,3 +1,12 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        'Closes the application'
+        Me.Close()
 
+    End Sub
+
+    Private Sub btnOk_Click(sender As Object, e As EventArgs) Handles btnOk.Click
+        'Mostra uma caixa de texto ao ser clicado'
+        MessageBox.Show("Olá " & txtName.Text & ", welcome to this brandnew application!!", "Message to the user")
+    End Sub
 End Class
